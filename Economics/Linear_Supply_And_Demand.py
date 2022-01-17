d9eb7b9c4edb36eb22dbf0e004d7c67d7c602310
ColderThanSeptember <80278580+DevinAdvani@users.noreply.github.com>
2022-01-17 22:28:29 +0000

    lol idk

--- Economics/Linear_Supply_And_Demand.py
+++ Economics/Linear_Supply_And_Demand.py
@@ -1,4 +1,3 @@
-
 #Imports
 import matplotlib.pyplot as plt
 import numpy as np
--- Economics/Tax_and_Subsidy.py
+++ Economics/Tax_and_Subsidy.py
@@ -1,1 +1,35 @@
-test
+#Imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Inputs
+PED, DI, PES, SI = float(input("Enter PED (Negative): ")),float(input("Enter Demand Intercept With Y Axis (Positive): ")),float(input("Enter PES (Positive): ")),float(input("Enter Supply Intercept With Y Axis (Either): "))
+
+#Calculating Q and P
+Q = (SI-DI)/(PED-PES)
+P = (PED * Q) + DI
+
+#Printing information
+print(f'QUANTITY = {Q} UNITS')
+print(f"PRICE = £{P}")
+print(f"TOTAL REVENUE = £{P*Q}")
+
+#Idk what these functions do, I copied the internet
+fig= plt.figure()
+axes= fig.add_axes([0.1,0.1,0.8,0.8])
+x= np.arange(0,100)
+
+#Plotting Supply and Demand
+axes.plot(x,((PES * x)+ SI))
+axes.plot(x,((PED * x)+ DI))
+
+#Plotting Price and Quantity
+axes.hlines(y=P, xmin=0, xmax=Q, linestyles = '--', colors = 'black')
+axes.vlines(x=Q, ymin=0, ymax=P, linestyles = '--', colors = 'black')
+
+#x and y range
+axes.set_xlim([0,(-DI/PED)+1])
+axes.set_ylim([0,DI+1])
+
+#Showing plot
+plt.show()
