ac9b796a9bdb1b1d55a50fd4f2bdd85b75cca4eb
ColderThanSeptember <80278580+DevinAdvani@users.noreply.github.com>
2022-01-17 22:23:07 +0000

    Basically Finished Supply and Demand

--- Economics/Linear_Supply_And_Demand.py
+++ Economics/Linear_Supply_And_Demand.py
@@ -1,10 +1,10 @@
+
 #Imports
 import matplotlib.pyplot as plt
 import numpy as np
 
-'''
 #Inputs
-PED, PES, DI, SI = float(input("Enter PED (Negative): ")),float(input("Enter PES (Positive): ")),float(input("Enter Demand Intercept With Y Axis (Positive): ")),float(input("Enter Supply Intercept With Y Axis (Either): "))
+PED, DI, PES, SI = float(input("Enter PED (Negative): ")),float(input("Enter Demand Intercept With Y Axis (Positive): ")),float(input("Enter PES (Positive): ")),float(input("Enter Supply Intercept With Y Axis (Either): "))
 
 #Calculating Q and P
 Q = (SI-DI)/(PED-PES)
@@ -15,14 +15,22 @@ print(f'QUANTITY = {Q} UNITS')
 print(f"PRICE = £{P}")
 print(f"TOTAL REVENUE = £{P*Q}")
 
-'''
-#plotting graphs
-x = np.linspace(0,10,10) # second and third variable should depend on graph size - plots graph size
-y = 2*x+1
-plt.plot(x, y, '-r', label='y=2x+1')
-plt.title('Graph of y=2x+1')
-plt.xlabel('x', color='#1C2833')
-plt.ylabel('y', color='#1C2833')
-plt.legend(loc='upper left')
-plt.grid()
+#Idk what these functions do, I copied the internet
+fig= plt.figure()
+axes= fig.add_axes([0.1,0.1,0.8,0.8])
+x= np.arange(0,100)
+
+#Plotting Supply and Demand
+axes.plot(x,((PES * x)+ SI))
+axes.plot(x,((PED * x)+ DI))
+
+#Plotting Price and Quantity
+axes.hlines(y=P, xmin=0, xmax=Q, linestyles = '--', colors = 'black')
+axes.vlines(x=Q, ymin=0, ymax=P, linestyles = '--', colors = 'black')
+
+#x and y range
+axes.set_xlim([0,(-DI/PED)+1])
+axes.set_ylim([0,DI+1])
+
+#Showing plot
 plt.show()
