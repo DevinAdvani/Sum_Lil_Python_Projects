f2c9783ebb582626fc4c6298f4d3355dcfd4a22f
ColderThanSeptember <80278580+DevinAdvani@users.noreply.github.com>
2022-03-18 19:19:03 +0000

    yes

---
+++ Bad_Economics_Project/1-Theories.py
@@ -0,0 +1,16 @@
+#Assumptions
+RE = False # Rational Expectations
+RCHHIR = True # Respective Country has the highest interest rates
+CIS = True # Country is stable
+GGDCMC = True # Governments generally don't cause Massive Change Unless Pressured
+
+# Interest Rates cause an effect on imports and exports
+
+if RCHHIR = True:
+    if CIS = True:
+        print("Increasing Interest Rates will have an effect upon imports and exports")# This is a bad, underone but sufficient example of the kind of concept that I am talking about that could get a lot more impressive and complex if someone spent their life into this.
+
+# The concept that governments will not deal with climate change until living standards drastically decrease in first world countries
+
+if GGDCMC = True:
+    print("Climate Change will not dealt with until living standards drastically decrease for those in first world countries")# Obviously there are other assumptions that I haven't mentioned that one can probably think of.
---
+++ Bad_Economics_Project/2-Historical_Data.py
@@ -0,0 +1,3 @@
+#Historical data such as the financial crash
+#GDP
+#That one time PED was positive because the two goods to buy in ireland were meat and potatoes and because potatoes were more important than meat to buy, if the price of potatoes increased meat could not be bought, therefore demand for potatoes increased. As a consequence PED was positive.
