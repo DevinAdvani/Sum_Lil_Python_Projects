55e4cd9b63ed3714c80251b62c215c1eef8960f6
ColderThanSeptember <80278580+DevinAdvani@users.noreply.github.com>
2022-03-18 23:59:19 +0000

    yes

---
+++ Project_Euler_General_Solution_Functions/5.py
@@ -0,0 +1,17 @@
+'''
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+'''
+def f(x):#where x is the highest number
+    list = []
+    if x % 2 == 0:
+        for i in range(int(x/2 + 1), x+1):
+            list.append(i)
+    if x % 2 == 1:
+        for i in range(round(x/2)+1,x+1):
+            list.append(i)
+    print(list)
+print(f(37))
+
+#need to divide list element into prime factors and then find lowest common multiple
