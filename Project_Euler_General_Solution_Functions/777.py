d98ec99bd79ba411bfea3af41714bd608e67e4cb
ColderThanSeptember <80278580+DevinAdvani@users.noreply.github.com>
2022-03-26 18:49:02 +0000

    fuck yh

--- Project_Euler_General_Solution_Functions/777.py
+++ Project_Euler_General_Solution_Functions/777.py
@@ -6,32 +6,37 @@ n=100
 a = 7
 b = 4
 for t in range(0,360*n + 1):
-    x.append(round(math.cos(a*((t/n)*math.pi/180)),7))
-    y.append(round(math.cos(b*(((t/n)*math.pi/180)-(pi/10))),7))
+    x.append(math.cos(a*t*(1/n)*math.pi*(1/180)))
+    y.append(math.cos(b*(((t/n)*(math.pi/180))-(math.pi/10))))
 plt.scatter(x, y)
 plt.show()
 
 def m(x1,y1,x2,y2):
+    g = (y2-y1)/(x2-x1)
+    return g
+
+def c(x1,y1,x2,y2):
     m = (y2-y1)/(x2-x1)
-    c = y1 - m*x1
-    return m
+    h = y1 - (m*x1)
+    return h
 
-def m(x1,y1,x2,y2):
-    m = (y2-y1)/(x2-x1)
-    c = y1 - m*x1
-    return c
-
-
-def d(a,b):
+def d():
     sum = 0
-    a = []
-    b = []
-    for c in range(0,360*n + 1):
-        for d in range(0,360*n + 1):
-            print(c,d)
-            if c != d and x[c] == x[d] and y[c] == y[d]:
-                a.append(x[c])
-                b.append(y[c])
-    for i in range(0,len(a)):
-        sum += (a[i]**2 + b[i]**2)
-        return sum
+    xic = []#x intersect coordinates
+    yic = []#y intersect coordinates
+    for e in range(0,360*n):
+        for d in range(0,360*n):
+            m1 = m(x[e],y[e],x[e+1],y[e+1])
+            c1 = c(x[e],y[e],x[e+1],y[e+1])
+            m2 = m(x[d],y[d],x[d+1],y[d+1])
+            c2 = c(x[d],y[d],x[d+1],y[d+1])
+            if m1 == m2:
+                xinput = 0
+            else:
+                xinput = (c2-c1)/(m1-m2)
+            if e != d and m1*xinput + c1 == m2*xinput + c2:#add the intersect
+                xic.append(xinput)
+                yic.append(m1*xinput + c1)
+        return xic
+
+print(d())
