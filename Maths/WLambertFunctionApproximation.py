e758579ea7340bf9e2d4a1b4987e5ee69dab7904
ColderThanSeptember <80278580+DevinAdvani@users.noreply.github.com>
2022-06-12 16:52:26 +0100

    yes

--- Maths/WLambertFunctionApproximation.py
+++ Maths/WLambertFunctionApproximation.py
@@ -1,1 +1,4 @@
 #The concept that the W lambert function or x = ye^y can be approximate by  a * ln(P(x)+1) = y
+#for x > 0
+#or maybe a log of a different base
+#the best I have manually found is y = 1.4log(1.5x + 1)
